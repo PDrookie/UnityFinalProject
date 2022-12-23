ec5a482513e7817f3053af1ece594230ab54df42
s98420 <119891540+s98420@users.noreply.github.com>
2022-12-23 18:51:57 +0800

    debug

--- project/Assets/Scripts/PauseGame.cs
+++ project/Assets/Scripts/PauseGame.cs
@@ -5,18 +5,31 @@ using UnityEngine.UI;
 
 public class PauseGame : MonoBehaviour
 {
+    [SerializeField] private GameObject panel;
+    private GameObject maincamera;
+    private bool ESCpress;
     // Start is called before the first frame update
-
-
+    private void Start()
+    {
+        maincamera = GameObject.Find("Main Camera");
+        ESCpress = false;
+    }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape) && !ESCpress)
         {
-            
+            ESCpress = true;
             Time.timeScale = 0;
-            GameObject.Find("Stop").SetActive(true);
-            GameObject.Find("Main Camera").GetComponent<RapidBlurEffect>().enabled = true;
+            panel.SetActive(true);
+            maincamera.GetComponent<RapidBlurEffect>().enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && ESCpress)
+        {
+            ESCpress = false;
+            Time.timeScale = 1;
+            panel.SetActive(false);
+            maincamera.GetComponent<RapidBlurEffect>().enabled = false;
         }
     }
 }
