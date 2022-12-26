59fee2390a649d46da38b9eea9960eadb3d7499c
s98420 <119891540+s98420@users.noreply.github.com>
2022-12-27 03:00:17 +0800

    角色移動 地圖邊緣 相機跟隨
    優化角色移動
    把邊緣磨平
    相機跟隨角色

--- project/Assets/Scripts/SwitchCamera.cs
+++ project/Assets/Scripts/SwitchCamera.cs
@@ -9,43 +9,51 @@ public class Constants
 }
 public class SwitchCamera : MonoBehaviour
 {
-    [SerializeField] private CustomArrays[] CamArray; //default 5x5 Array
+    //[SerializeField] private CustomArrays[] CamArray; //default 5x5 Array
     [SerializeField] private Transform Player;
+    [SerializeField] private float HorCameraDistance;
+    [SerializeField] private float VerCameraDistance;
+    [SerializeField]private Camera MainCam;
     private float PlayerX;
+    private float PlayerY;
     private float DistanceX;
-    private Camera CamNow;
-    int CamNowLocationX;
-    int CamNowLocationY;
+    private float DistanceY;
     void Start()
     {
+        Vector3 PlayerPos = Player.position;
+        MainCam.transform.position = new Vector3(PlayerPos.x, PlayerPos.y, -10);
         PlayerX = Player.position.x;
-        CamNow = CamArray[(Constants.MapSize + 1) / 2][(Constants.MapSize + 1) / 2];
-        CamNowLocationX = (Constants.MapSize + 1) / 2;
-        CamNowLocationY = (Constants.MapSize + 1) / 2;
+        PlayerY = Player.position.y;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
         PlayerX = Player.position.x;
-        DistanceX = PlayerX - CamNow.transform.position.x;
-        Debug.Log(DistanceX);
-        if (DistanceX > 11.4)
+        PlayerY = Player.position.y;
+        DistanceX = PlayerX - MainCam.transform.position.x;
+        DistanceY = PlayerY - MainCam.transform.position.y;
+        if (DistanceX > HorCameraDistance)
         {
-            CamArray[CamNowLocationX][CamNowLocationY].gameObject.SetActive(false);
-            CamArray[CamNowLocationX + 1][CamNowLocationY].gameObject.SetActive(true);
-            CamNow = CamArray[CamNowLocationX + 1][CamNowLocationY];
-            CamNowLocationX++;
+            MainCam.transform.position = new Vector3(MainCam.transform.position.x + HorCameraDistance * 2, MainCam.transform.position.y, MainCam.transform.position.z);
         }
-        else if (DistanceX < -11.4)
+        else if (DistanceX < -HorCameraDistance)
         {
-            CamArray[CamNowLocationX][CamNowLocationY].gameObject.SetActive(false);
-            CamArray[CamNowLocationX - 1][CamNowLocationY].gameObject.SetActive(true);
-            CamNow = CamArray[CamNowLocationX - 1][CamNowLocationY];
-            CamNowLocationX--;
+            MainCam.transform.position = new Vector3(MainCam.transform.position.x - HorCameraDistance * 2, MainCam.transform.position.y, MainCam.transform.position.z);
+        }
+        if (DistanceY > VerCameraDistance)
+        {
+            MainCam.transform.position = new Vector3(MainCam.transform.position.x, MainCam.transform.position.y + VerCameraDistance * 2, MainCam.transform.position.z);
+        }
+        else if (DistanceY < -VerCameraDistance)
+        {
+            MainCam.transform.position = new Vector3(MainCam.transform.position.x, MainCam.transform.position.y - VerCameraDistance * 2, MainCam.transform.position.z);
         }
     }
 }
+
+
+
 [System.Serializable]
 public class CustomArrays
 {
