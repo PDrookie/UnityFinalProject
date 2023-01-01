f1635cc13a251ed178686c87b6bdec6487479f72
s98420 <119891540+s98420@users.noreply.github.com>
2023-01-01 18:39:05 +0800

    武器

---
+++ project/Assets/Scripts/Weapon/WeaponAni.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class WeaponAni : MonoBehaviour
+{
+    private Animator Ani;
+    private Animator playerAni;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerAni = GameObject.Find("Player").GetComponent<Animator>();
+        Ani = transform.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void AniFinish()
+    {
+        transform.gameObject.SetActive(false);
+        Ani.SetBool("IsAttack", false);
+        playerAni.SetBool("IsAttack", false);
+    }
+}
